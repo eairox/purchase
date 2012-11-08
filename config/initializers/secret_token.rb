b42c8dc2307f6cbee3a0f738d24bfa657b2111bc
Umesh Garg <eairox@gmail.com>
2012-11-08 06:37:56 -0800

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purchasistry::Application.config.secret_token = 'b77798da0774214df8af787379cb95a947d81da1c35bbb6ba75c274ad375aa96cf0d4b6f97d358fdf8040a9d9bc4d0a0ddeccb45e36d4bb9adaa80800f2d3012'
